5b884820ffea6bf87d5e1ab2cd0a8f524055b624
Ancita Andrade <ancitaandrade@gmail.com>
2025-03-05 15:16:14 -0500

    Added comments

--- assignment2.py
+++ assignment2.py
@@ -8,22 +8,27 @@ def read_input_file(filename):
         print(f"Error reading file {filename}: {e}")
         exit(1)
 
+#function which will calculate the maximum weight of the sticks that can be put in the tube
 def sticks_and_weights(n, L, l_n, w_n):
     weight = 0
     sticks = []
     ratio_dict = {}
+    #Heuristic: Sort the sticks by their weight to length ratio and storing it in a dictionary where key is the index of the stick
     for i in range(n):
         ratio_dict[i] = w_n[i] / l_n[i]
+    #Sorting the dictionary by the ratio of weight to length
     sorted_dict = sorted(ratio_dict.items(), key=lambda item: item[1], reverse=True)
 
+    #iterating through each stick in the dictionary
     for key, _ in sorted_dict:
         currentLength = l_n[key]
         if L == 0:
             break
+        #if the current length of the stick is less than the remaining length of the tube, add the weight of the stick to the total weight
         if L >= currentLength:
             L -= currentLength
             weight += w_n[key]
-        else:
+        else: #if the current length of the stick is greater than the remaining length of the tube, add the fraction of the stick and it's weight to the total weight
             weight += w_n[key] * (L / currentLength)
             L = 0
         sticks.append(key+1)
